1012a699ba33925088ceea8d4d32de75c870eb36
cudjoeab <cudjoeab@gmail.com>
2019-07-09 14:36:21 -0400

    exercise 2 complete

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,4 @@
 print(2) 
 print(3) 
 print(2+3)
+
---
+++ exercise2.py
@@ -0,0 +1,16 @@
+# 1. calculate tip on a meal 
+
+print (55/0.80)
+
+#2. concatenating string and integers 
+print ("Your total is " + str(55/0.80))
+
+#3. string interpolation 
+print ("45628 multiplied by 7839 equals " + str(45628 * 7839) + "!")
+
+#4. Finding the value of an expression
+    # prediction: (10 < 20 and 30 < 20) or not(10 == 11)
+                # (true and false) or true
+                #  false or true
+                #  true 
+print ((10 < 20 and 30 < 20) or not(10 == 11))
