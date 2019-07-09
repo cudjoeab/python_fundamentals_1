f6c5354c7fdcc4c80497afc84096dc69f3cb4dd7
cudjoeab <cudjoeab@gmail.com>
2019-07-09 16:35:27 -0400

    elif extra on tipping

---
+++ tipping.py
@@ -0,0 +1,10 @@
+
+#info from input is stored in the variable
+tip = float(input())
+
+if tip <= 0.10:
+    print("Thanks.")
+elif tip <= 0.15:
+    print("somethinghappier")
+else: 
+    print('validation')
