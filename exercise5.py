0a655b33f59802a837252377909fab1011bfa497
cudjoeab <cudjoeab@gmail.com>
2019-07-09 18:22:30 -0400

    partial completion of exercise 5

---
+++ exercise5.py
@@ -0,0 +1,24 @@
+# exercise tracker 
+
+total_distance = 0
+
+print("Would you like to walk or run?")
+action = input().lower()
+
+while total_distance < 12: 
+    if action == "walk":
+        total_distance += 1
+        print("Distance from home is {}km.".format(total_distance))
+    elif action == "run": 
+        total_distance += 5
+        print("Distance from home is {}km.".format(total_distance))
+    else: 
+        
+    
+
+
+
+
+
+
+
