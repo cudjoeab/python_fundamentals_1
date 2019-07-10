fdbb098c4115888ea37446b4dcfdc0a2366386a1
cudjoeab <cudjoeab@gmail.com>
2019-07-09 22:36:30 -0400

    exercise 6.1 go home

--- exercise5.py
+++ exercise5.py
@@ -1,34 +1,22 @@
 # # exercise tracker 
 
-# total_distance = 0
+total_distance = 0
 
+#ask user for input on what action to take - walk or run
 # print("Would you like to walk or run?")
 # action = input().lower()
 
-# while total_distance < 12: 
-#     if action == "walk":
-#         total_distance += 1
-#         print("Distance from home is {}km.".format(total_distance))
-#     elif action == "run": 
-#         total_distance += 5
-#         print("Distance from home is {}km.".format(total_distance))
-#     else: 
-
-
-total_distance = 0
-
-print("Would you like to walk or run?")
-action = input().lower()
-
-while total_distance < 20:
-    print("Distance from home is {} km.".format(total_distance))
-    total_distance += 1
+# while the user input gives a truthy, in this case action == walk or run
+while True: 
+    print("Would you like to walk or run?")
+    action = input().lower()
     if action == "walk":
         total_distance += 1
         print("Distance from home is {}km.".format(total_distance))
-        break 
     elif action == "run": 
         total_distance += 5
         print("Distance from home is {}km.".format(total_distance))
+    elif action == "go home": #allow the user to go home when done exercising
+        print("Great break! Let's go home!")
         break 
     
