6ed667f3be2e01817d450fc1b3d77b7df29aa408
cudjoeab <cudjoeab@gmail.com>
2019-07-09 23:49:19 -0400

    exercise 6.2, user can sprint, eat, and rest

--- exercise5.py
+++ exercise5.py
@@ -23,16 +23,26 @@ while True:
             energy -= 2
             print("Distance from home is {}km.".format(total_distance) + " Energy level is {}.".format(energy))
         else: 
-             print("You're too tired. Get some rest or have a snack.")
+             print("Energy level is {}.".format(energy) + "Get some rest or have a snack.")
+    elif action == "sprint":
+        if energy >= 5:
+            total_distance +=8
+            energy -= 5
+            print("Wow you're fast! " + "Distance is {} km. ".format(total_distance) + "Energy level is {}.".format(energy))
+        else: 
+            print("Can't speed up right now... " + "Energy level is {}.".format(energy))
     elif action == "rest":
         energy += 1
-        print("Great idea to take a breather.")
-    elif action == "eat":
+        print("Great idea to take a breather." + "Energy level is {}.".format(energy))
+    elif (action == "eat") or (action == "snack"): 
         energy += 3
-        print("Yes! Get your energy up!")
+        print("Yes! Get your energy up!" + "Energy level is {}.".format(energy))
     elif action == "go home": #allow the user to go home when done exercising
         print("Great break! Let's go home!")
         break 
+    else: 
+        print ("Your options are: go home\twalk\trun\tsprint\trest\teat or snack\t")
+        
 
 
 
